6ebb1a9998aedaf18991ba1b14580279c1aa1e9e
ChimeraLC <lucaschen314@gmail.com>
2024-03-01 21:57:50 -0600

    Reformatting

--- house.cpp
+++ house.cpp
@@ -5,7 +5,7 @@
 /* Library imports */
 #include <iostream>
 #include <stdlib.h>
-#include <glad/glad.h> 
+#include <glad/glad.h>
 #include <cmath>
 #include <GLFW/glfw3.h>
 
@@ -22,18 +22,19 @@
 using namespace std;
 
 /* Image file structure */
-typedef struct {
+typedef struct
+{
     int width;
     int height;
-    unsigned char* data;
+    unsigned char *data;
 } Image;
 
 /* Function Prototypes */
-void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void mouse_callback(GLFWwindow* window, double mouseX, double mouseY);
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void mouse_callback(GLFWwindow *window, double mouseX, double mouseY);
 void handleInput(GLFWwindow *window, float delta);
 int bind_texture(int height, int width, int glTexture);
-Image readBMP(char* filename);
+Image readBMP(char *filename);
 Image generate_texture(int height, int width);
 
 /* Window Settings */
@@ -41,17 +42,19 @@ const unsigned int SCR_WIDTH = 1600;
 const unsigned int SCR_HEIGHT = 1200;
 
 /* Camera Settings */
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));     // Camera instance
-float prevMouseX = SCR_WIDTH / 2;               // Place mouse initially in center of screen
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f)); // Camera instance
+float prevMouseX = SCR_WIDTH / 2;           // Place mouse initially in center of screen
 float prevMouseY = SCR_HEIGHT / 2;
-bool mouseFound = false;                        // If mouse has been moved
+bool mouseFound = false; // If mouse has been moved
 
 /* Rendering settings */
 const float FPS = 30.0f;
+const int GRID_WIDTH = 4;
 
 /* Main function */
 
-int main() {
+int main()
+{
 
     /* Initialize GLFW*/
     glfwInit();
@@ -60,7 +63,7 @@ int main() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Initialize window */
-    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Piranesi", NULL, NULL);
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Piranesi", NULL, NULL);
     /* Check for error during initialization */
     if (window == NULL)
     {
@@ -74,7 +77,7 @@ int main() {
     /* Binding callback functions. */
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glfwSetCursorPosCallback(window, mouse_callback);
-  
+
     // Capture mouse inputs using GLFW
     glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
@@ -83,36 +86,61 @@ int main() {
     {
         std::cout << "Error when initializing GLAD" << std::endl;
         return -1;
-    } 
+    }
 
     /* Building and compiling shaders */
     Shader mainShader("shaders/houseshader.vs", "shaders/houseshader.fs");
 
     /* Enable vertex depth */
-    glEnable(GL_DEPTH_TEST);  
-
+    glEnable(GL_DEPTH_TEST);
 
     /* Vertex data for ground */
     float groundVertices[] = {
         // Position           // Texture         // Normal
-        -0.5f, -0.5f, -0.5f,  0.0f, 0.0f, 0.0f,  1.0f, 0.0f,
-         0.5f, -0.5f, -0.5f,  1.0f, 0.0f, 0.0f,  1.0f, 0.0f,
-         0.5f, -0.5f, 0.5f,   1.0f, 1.0f, 0.0f,  1.0f, 0.0f,
-        -0.5f, -0.5f, 0.5f,   0.0f, 1.0f, 0.0f,  1.0f, 0.0f,
+        -0.5f,
+        -0.5f,
+        -0.5f,
+        0.0f,
+        0.0f,
+        0.0f,
+        1.0f,
+        0.0f,
+        0.5f,
+        -0.5f,
+        -0.5f,
+        1.0f,
+        0.0f,
+        0.0f,
+        1.0f,
+        0.0f,
+        0.5f,
+        -0.5f,
+        0.5f,
+        1.0f,
+        1.0f,
+        0.0f,
+        1.0f,
+        0.0f,
+        -0.5f,
+        -0.5f,
+        0.5f,
+        0.0f,
+        1.0f,
+        0.0f,
+        1.0f,
+        0.0f,
     };
 
     /* Indices for ground */
     unsigned int groundIndices[] = {
         0, 1, 3,
-        1, 2, 3
-    };
+        1, 2, 3};
 
     /* Vertex array and buffer objects, alongside element buffer objects. */
     unsigned int VBO[1], VAO[1], EBO[1];
     glGenVertexArrays(1, VAO);
     glGenBuffers(1, VBO);
     glGenBuffers(1, EBO);
-
 
     /* Binding VAO used for the ground */
     glBindVertexArray(VAO[0]);
@@ -125,23 +153,22 @@ int main() {
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(groundIndices), groundIndices, GL_STATIC_DRAW);
 
     // Getting position vectors
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)0);
     glEnableVertexAttribArray(0);
 
     // Getting texture vectors
-    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
-    // Getting normal vectors 
-    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(5 * sizeof(float)));
+    // Getting normal vectors
+    glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *)(5 * sizeof(float)));
     glEnableVertexAttribArray(2);
-
 
     /* Configuring shader textures */
     mainShader.use();
 
     /* Generate texture for ground */
-    bind_texture(1024, 1024, GL_TEXTURE0);
+    bind_texture(128, 128, GL_TEXTURE0);
 
     /* Timing of frames */
     float delta = 0.0f;
@@ -173,13 +200,13 @@ int main() {
             mainShader.use();
             glm::mat4 model = glm::mat4(1.0f);
             glm::mat4 view = camera.GetViewMatrix();
-            glm::mat4 projection    = glm::mat4(1.0f);
+            glm::mat4 projection = glm::mat4(1.0f);
             projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
 
             /* Store transformation data locations */
             unsigned int modelLoc = glGetUniformLocation(mainShader.ID, "model");
-            unsigned int viewLoc  = glGetUniformLocation(mainShader.ID, "view");
-            unsigned int projLoc  = glGetUniformLocation(mainShader.ID, "projection");
+            unsigned int viewLoc = glGetUniformLocation(mainShader.ID, "view");
+            unsigned int projLoc = glGetUniformLocation(mainShader.ID, "projection");
 
             /* View and projection values are constant */
             glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
@@ -195,23 +222,28 @@ int main() {
             glUniform1i(glGetUniformLocation(mainShader.ID, "textureID"), 0);
 
             /* Always center ground right below camera */
+            float cameraGridX, cameraGridZ; // World is split into grid_size x grid_size squares
+            glm::vec3 cameraPos = camera.GetPosition();
+            cameraGridX = floor(cameraPos.x / GRID_WIDTH);
+            cameraGridZ = floor(cameraPos.z / GRID_WIDTH);
 
             /* Generate transitions */
-            for (int i = -5; i <= 5; i++) {
-                for (int j = -5; j <= 5; j++) {
+            for (int i = cameraGridX - 2; i <= cameraGridX + 2; i++)
+            {
 
-                    model = glm::mat4(1.0f);
-                    model = glm::translate(model, glm::vec3(i, -2.0f, j));
+                model = glm::mat4(1.0f);
+                /* Translate to be along gridlines */
+                model = glm::translate(model, glm::vec3(GRID_WIDTH / 2, -2.0f, GRID_WIDTH / 2));
 
-                    /* Resize */
-                    model = glm::scale(model, glm::vec3(5, 1.0, 5));
-                    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+                /* Translate to player */
+                model = glm::translate(model, glm::vec3(GRID_WIDTH * i, -2.0f, GRID_WIDTH * cameraGridZ));
 
-                    /* Draw triangle*/
-                    if (i == 0 && j == 0)
-                    glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
+                /* Resize */
+                model = glm::scale(model, glm::vec3(GRID_WIDTH, 1.0, GRID_WIDTH));
+                glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
-                }
+                /* Draw triangle*/
+                glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
             }
             /* Swap buffers and poll events */
             glfwSwapBuffers(window);
@@ -223,7 +255,7 @@ int main() {
     glDeleteVertexArrays(1, VAO);
     glDeleteBuffers(1, VBO);
     glDeleteBuffers(1, EBO);
-    
+
     /* Terminate glfw */
     glfwTerminate();
 
@@ -233,8 +265,8 @@ int main() {
 /**
  * Requires:
  *      Window is a properly initialized GLFW window.
- *      Camera has been propertly initialized. 
- * 
+ *      Camera has been propertly initialized.
+ *
  * Effects:
  *  Handles the user input for the given window.
  */
@@ -247,22 +279,33 @@ void handleInput(GLFWwindow *window, float delta)
     glm::vec2 move_inputs;
 
     /* Movement inputs */
-    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+    {
         move_inputs.y += 1;
     }
-    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+    {
         move_inputs.x += 1;
     }
-    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+    {
         move_inputs.y -= 1;
     }
-    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+    {
         move_inputs.x -= 1;
     }
 
     /* Normalize*/
-    if (move_inputs.x != 0 || move_inputs.y != 0) {
+    if (move_inputs.x != 0 || move_inputs.y != 0)
+    {
         move_inputs = glm::normalize(move_inputs);
+    }
+
+    /* Sprinting */
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_RIGHT_SHIFT) == GLFW_PRESS)
+    {
+        move_inputs *= 3;
     }
 
     /* Scale by time */
@@ -272,30 +315,30 @@ void handleInput(GLFWwindow *window, float delta)
     camera.ProcessInputs(move_inputs);
 }
 
-/* 
+/*
  *  Requires:
  *      This function should be registered by glfwSetFramebufferSizeCallback.
- * 
+ *
  *  Effects:
  *      This function is called whenever the window is resized.
  */
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
-    (void) window; // Unusued parameter
+    (void)window; // Unusued parameter
     glViewport(0, 0, width, height);
 }
 
-/* 
+/*
  *  Requires:
  *      This function should be registered by glfwSetCursorPosCallback.
- * 
+ *
  *  Effects:
  *      This function is called whenever the cursor position is moved.
  */
-void mouse_callback(GLFWwindow* window, double mouseX, double mouseY)
+void mouse_callback(GLFWwindow *window, double mouseX, double mouseY)
 {
     /* Unused window parameter */
-    (void) window;
+    (void)window;
 
     /* Cast positions */
     float xpos = static_cast<float>(mouseX);
@@ -321,11 +364,11 @@ void mouse_callback(GLFWwindow* window, double mouseX, double mouseY)
     camera.ProcessMouseMovement(xoffset, yoffset);
 }
 
-/* 
+/*
  *  Requires:
  *      The width and height should be positive values and glTexture an integer
  *      corresponding to a gl texture.
- * 
+ *
  *  Effects:
  *      Reads the bmp file binds it to the given gl texture.
  */
@@ -348,11 +391,13 @@ int bind_texture(int height, int width, int glTexture)
     std::cout << "Reading texture with width " << width << " and height " << height << std::endl;
 
     /* If succesfully loaded */
-    if (data) {
+    if (data)
+    {
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, data);
         glGenerateMipmap(GL_TEXTURE_2D);
     }
-    else {
+    else
+    {
         std::cout << "Error when loading texture data" << std::endl;
         return -1;
     }
@@ -363,42 +408,62 @@ int bind_texture(int height, int width, int glTexture)
 /*
  *  Requires:
  *      The width and height should be positive values.
- * 
+ *
  *  Effects:
- *      Generates a random texture with the given width and height 
+ *      Generates a random texture with the given width and height
  * */
 Image generate_texture(int height, int width)
 {
     /* Create corresponding image size*/
-    unsigned char* data = (unsigned char*) calloc(3 * width * height, sizeof(unsigned char));
+    unsigned char *data = (unsigned char *)calloc(3 * width * height, sizeof(unsigned char));
 
     /* Create perlin noise */
     Perlin perlin = Perlin(height, width);
 
     /* Marble Colors*/
     int marble_dark[3] = {180, 190, 195};
+    // int marble_dark[3] = {0, 0, 0};
     int marble_light[3] = {205, 224, 227};
 
-
     /* Generate internals */
-    for (int row = 0; row < height; row++) {
-        for (int col = 0; col < width; col ++) {
+    for (int row = 0; row < height / 2; row++)
+    {
+        for (int col = 0; col < width / 2; col++)
+        {
             /* Calculate noise */
-            double noise = perlin.Perlin_Val((double) col, (double) row);
+            double noise = perlin.Perlin_Marble((double)col, (double)row);
 
             /* Expand dark and light values */
-            double  light_noise = noise,
-                dark_noise = 1 - noise;
-            
+            double light_noise = noise,
+                   dark_noise = 1 - noise;
+
             /* Blue */
             data[3 * (row * width + col)] =
-                (unsigned char) (dark_noise * marble_dark[0] + light_noise * marble_light[0]);
+                (unsigned char)(dark_noise * marble_dark[0] + light_noise * marble_light[0]);
+            data[3 * ((height - 1 - row) * width + col)] =
+                data[3 * (row * width + col)];
+            data[3 * (row * width + (width - 1 - col))] =
+                data[3 * (row * width + col)];
+            data[3 * ((height - 1 - row) * width + (width - 1 - col))] =
+                data[3 * (row * width + col)];
             /* Green */
             data[3 * (row * width + col) + 1] =
-                (unsigned char) (dark_noise * marble_dark[1] + light_noise * marble_light[1]);
+                (unsigned char)(dark_noise * marble_dark[1] + light_noise * marble_light[1]);
+            data[3 * ((height - 1 - row) * width + col) + 1] =
+                data[3 * (row * width + col) + 1];
+            data[3 * (row * width + (width - 1 - col)) + 1] =
+                data[3 * (row * width + col) + 1];
+            data[3 * ((height - 1 - row) * width + (width - 1 - col)) + 1] =
+                data[3 * (row * width + col) + 1];
             /* Red */
             data[3 * (row * width + col) + 2] =
-                (unsigned char) (dark_noise * marble_dark[2] + light_noise * marble_light[2]);
+                (unsigned char)(dark_noise * marble_dark[2] + light_noise * marble_light[2]);
+            data[3 * ((height - 1 - row) * width + col) + 2] =
+                data[3 * (row * width + col) + 2];
+            data[3 * (row * width + (width - 1 - col)) + 2] =
+                data[3 * (row * width + col) + 2];
+            data[3 * ((height - 1 - row) * width + (width - 1 - col)) + 2] =
+                data[3 * (row * width + col) + 2];
         }
     }
 
@@ -410,4 +475,3 @@ Image generate_texture(int height, int width)
 
     return image;
 }
-
--- perlin.h
+++ perlin.h
@@ -10,94 +10,117 @@ using namespace std;
  */
 class Perlin
 {
-    public:
-        int Primes[15] = {
-            7654567, 5195977, 1241087,
-            1866091, 3041789, 3069863,
-            1237547, 7794467, 7956589,
-            2495443, 4260917, 7004497,
-            1178711, 1600061, 9980869,
-        };
-        int Prime_index = 0;
-        int Height;
-        int Width;
+public:
+    int Primes[15] = {
+        7654567,
+        5195977,
+        1241087,
+        1866091,
+        3041789,
+        3069863,
+        1237547,
+        7794467,
+        7956589,
+        2495443,
+        4260917,
+        7004497,
+        1178711,
+        1600061,
+        9980869,
+    };
+    int Prime_index = 0;
+    int Height;
+    int Width;
 
-        /*
-         * Constructor that creates the Perlin noise class.
-         */
-        Perlin(int height, int width) {
-            Height = height;
-            Width = width;
+    /*
+     * Constructor that creates the Perlin noise class.
+     */
+    Perlin(int height, int width)
+    {
+        Height = height;
+        Width = width;
+    }
+
+    /* Interpolates between two values */
+    double Interpolate(double a, double b, double w)
+    {
+        /* Clamping */
+        w = w > 1 ? 1 : w;
+        w = w < 0 ? 0 : w;
+
+        /* Calculate interpolation */
+        // return (a-b) * w + b;
+        return (b - a) * w + a;
+    }
+
+    /*
+     * Generates pseudorandom noise value between 0 and 2pi
+     */
+    double Noise(int i, int x, int y)
+    {
+        int n = x + y * 59;
+        n = (n << 13) ^ n;
+        int a = Primes[i * 3], b = Primes[i * 3 + 1], c = Primes[i * 3 + 2];
+        int t = (n * (n * n * a + b) + c) & 0x3fffffff;
+        double rand = 2.0 - (double)(t) / (1 << 29);
+        return rand * 3.14159265;
+    }
+
+    /*
+     * Calculates the dot product with the gradient
+     */
+    double Gradient(int i, int ix, int iy, double x, double y)
+    {
+        /* Calculate noise */
+        double noise = Noise(i, ix, iy);
+
+        /* Calculate distances */
+        double dx = x - (double)ix;
+        double dy = y - (double)iy;
+
+        /* Return dot product */
+        return (dx * cos(noise) + dy * sin(noise)) * 0.5 + 0.5;
+    }
+
+    /*
+     * Calculates perlin at a given location
+     */
+    double Calc_Perlin(int i, double x, double y)
+    {
+
+        /* Grid points */
+        int x0 = (int)floor(x);
+        int x1 = x0 + 1;
+        int y0 = (int)floor(y);
+        int y1 = y0 + 1;
+
+        /* Interpolation values */
+        double fx = x - (double)x0;
+        double fy = y - (double)y0;
+
+        double i1 = Interpolate(Gradient(i, x0, y0, x, y),
+                                Gradient(i, x1, y0, x, y), fx);
+        double i2 = Interpolate(Gradient(i, x0, y1, x, y),
+                                Gradient(i, x1, y1, x, y), fx);
+
+        return Interpolate(i1, i2, fy);
+    }
+
+    double Perlin_Val(double x, double y)
+    {
+        double total = 0;
+        double ampl = 1;
+        for (int i = 0; i <= 4; i++)
+        {
+            total += Calc_Perlin(i, x / (1 << (4 + i)), y / (1 << (4 + i))) / ampl;
+            ampl *= 2;
         }
+        return total;
+    }
 
-        /* Interpolates between two values */
-        double Interpolate(double a, double b, double w) {
-            /* Clamping */
-            w = w > 1 ? 1 : w;
-            w = w < 0 ? 0 : w;
-
-            /* Calculate interpolation */
-            return (b - a) * w + a;
-        }
-
-        /* 
-         * Generates pseudorandom noise value between 0 and 2pi
-         */
-        double Noise(int i, int x, int y) {
-            int n = x + y * 59;
-            n = (n << 13) ^ n;
-            int a = Primes[i * 3], b = Primes[i * 3 + 1], c = Primes[i * 3 + 2];
-            int t = (n * (n * n * a + b) + c) & 0x3fffffff;
-            double rand = 2.0 - (double)(t)/(1 << 29);
-            return rand * 3.14159265;
-        }
-
-        /*
-         * Calculates the dot product with the gradient
-         */
-        double Gradient(int i, int ix, int iy, double x, double y) {
-            /* Calculate noise */
-            double noise = Noise(i, ix, iy);
-
-            /* Calculate distances */
-            double dx = x - (double) ix;
-            double dy = y - (double) iy;
-
-            /* Return dot product */
-            return (dx * cos(noise) + dy * sin(noise)) * 0.5 + 0.5;
-        }
-
-        /*
-         * Calculates perlin at a given location
-         */
-        double Calc_Perlin(double x, double y) {
-            int i = 1;
-
-            /* Grid points */
-            int x0 = (int) floor(x);
-            int x1 = x0 + 1;
-            int y0 = (int) floor(y);
-            int y1 = y0 + 1;
-
-            /* Interpolation values */
-            double fx = x - (double) x0;
-            double fy = y - (double) y0;
-        
-            double i1 = Interpolate(Gradient(i, x0, y0, x, y),
-                Gradient(i, x1, y0, x, y), fx);
-            double i2 = Interpolate(Gradient(i, x0, y1, x, y),
-                Gradient(i, x1, y1, x, y), fx);
-
-            return Interpolate(i1, i2, fy);
-        }
-
-        double Perlin_Val(double x, double y) {
-            double total = 0; 
-            for (int i = 0; i <= 0; i++) {
-                total += Calc_Perlin(x / (1 << (7 + i)), y / (1 << (7 + i)));
-            }
-            return total;
-        }
-
-
+    double Perlin_Marble(double x, double y)
+    {
+        double total = Perlin_Val(x, y);
+        return sin(2 * (total)) * 0.7 + 0.3;
+    }
 };
